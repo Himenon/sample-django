a1387af97816ee8048a3368473a867d5db9510d0
kosei.himeno <k.himeno314@gmail.com>
2018-03-08 10:55:21 +0900

    update: workerの整理/統合

--- taskmanager_v1/tm/tapp_b/tasks.py
+++ taskmanager_v1/tm/tapp_b/tasks.py
@@ -1,6 +1,6 @@
 from celery import shared_task
 from . import models as app_models
-
+from tapp_a import models as app_a_models
 
 @shared_task
 def hello_from_b(*args, **kwargs):
@@ -12,6 +12,15 @@ def hello_from_b(*args, **kwargs):
 @shared_task
 def counter_upper(counter_id, *args, **kwargs):
     m = app_models.Counter.objects.get(pk=counter_id)
+    m.count += 1
+    m.save()
+    print(m)
+    return m
+
+
+@shared_task
+def counter_upper_task_a_from_b(counter_id, *args, **kwargs):
+    m = app_a_models.Counter.objects.get(pk=counter_id)
     m.count += 1
     m.save()
     print(m)
